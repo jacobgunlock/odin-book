efde695e4bb9f3e20500d74af1db358a7d6051a9
jacobgunlock <jacobkgundlach@gmail.com>
2023-01-10 23:58:13 -0600

    array updating functionality

--- book.js
+++ book.js
@@ -3,10 +3,45 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        print(`${title} by ${author} has ${pages}. Read?: ${read}`)
-    }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
-theHobbit.info();
+const library = [];
+
+document.getElementById('add').addEventListener('click', (e) => {
+    e.preventDefault();
+    addBook();
+    displayLibrary();
+})
+
+function addBook() {
+    title = document.getElementById('title').value;
+    author = document.getElementById('author').value;
+    pages = document.getElementById('pages').value;
+    document.getElementById('read').checked
+    ? read = true
+    : read = false;
+    
+    const book = new Book(title, author, pages, read);
+    library.push(book);
+    console.log(library);
+}
+
+function displayLibrary() {
+    document.querySelector('#books').innerHTML = ''
+    library.forEach(book => {
+        const p = document.createElement('p');
+        const delBtn = document.createElement('button');
+        delBtn.setAttribute('class', 'del-btn');
+        delBtn.setAttribute('id', `${library.indexOf(book)}`);
+        delBtn.addEventListener('click', (deleteBook));
+        delBtn.textContent = 'delete';
+        p.innerText += `${book.title} by ${book.author}`
+        document.getElementById('books').appendChild(p);
+        p.appendChild(delBtn);
+    })
+}
+
+function deleteBook(e) {
+    library.splice(e.target.id, 1);
+    displayLibrary()
+}
